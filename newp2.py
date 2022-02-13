0bd8aa7f238bea6a10206d07182372b27353422f
sahil <sahilphulwani.sp@gmail.com>
2022-02-13 22:33:17 +0530

    score board fixed 2.0 & almost done

--- newp2.py
+++ newp2.py
@@ -10,11 +10,14 @@ abc =0
 
 x = turtle.Screen()
 x.title("Rock, Paper & Scissors")
-x.bgcolor("black")
+x.bgcolor("white")
 x.setup(1000,600)
+x.register_shape('rock.gif')
+x.register_shape('paper.gif')
+x.register_shape('scissors.gif')
 
 y = turtle.Turtle()
-y.color("white")
+y.color("black")
 y.speed(0)
 y.penup()
 y.goto(-490,280)
@@ -23,14 +26,26 @@ y.goto(-490,260)
 y.write("For Rock,Paper & Scissors Press R , P & S ",font=20)
 
 z = turtle.Turtle()
-z.color("white")
+z.color("black")
 z.speed(0)
 z.penup()
 z.goto(-490,220)
 z.write("Sahilpoint = {} ||  Computerpoint = {}".format(score,compscore),font= 80)
 
+myimg = turtle.Turtle()
+myimg.penup()
+myimg.speed(0)
+
+compimg = turtle.Turtle()
+compimg.penup()
+compimg.speed(0)
+
+
+
 def Rock():
     compinput= Glist[random.randint(0,2)]
+    myimg.goto(-250,0)
+    myimg.shape('rock.gif')
     global abc
     if compinput == "Rock":
          abc = 10
@@ -40,7 +55,9 @@ def Rock():
          y.goto(-490,260)
          y.write("For Rock,Paper & Scissors Press R , P & S ",font=20)
          y.goto(100,0)
-         y.write("You Won !!! ",font= 50)
+         compimg.goto(250,0)
+         compimg.shape('rock.gif')
+        #  y.write("You Won !!! ",font= 50)
     if compinput != "Rock": 
          abc = -10
          y.clear()
@@ -49,10 +66,19 @@ def Rock():
          y.goto(-490,260)
          y.write("For Rock,Paper & Scissors Press R , P & S ",font=20)
          y.goto(100,0)
-         y.write("Ooooppps! You Lose !",font= 50)
+         if compinput == "Paper":
+             compimg.goto(250,0)
+             compimg.shape('paper.gif')
+         else :
+             compimg.goto(250,0)
+             compimg.shape('scissors.gif')
+
+        #  y.write("Ooooppps! You Lose !",font= 50)
 
 def Paper():
      compinput= Glist[random.randint(0,2)]
+     myimg.goto(-250,0)
+     myimg.shape('paper.gif')
      global abc
      if compinput == "Paper":
          abc = 20
@@ -62,7 +88,9 @@ def Paper():
          y.goto(-490,260)
          y.write("For Rock,Paper & Scissors Press R , P & S ",font=20)
          y.goto(100,0)
-         y.write("You Won !!! ",font= 50) 
+         compimg.goto(250,0)
+         compimg.shape('paper.gif')
+        #  y.write("You Won !!! ",font= 50) 
      if compinput != "Paper":
          abc = -20
          y.clear()
@@ -71,10 +99,18 @@ def Paper():
          y.goto(-490,260)
          y.write("For Rock,Paper & Scissors Press R , P & S ",font=20)
          y.goto(100,0)
-         y.write("Ooooppps! You Lose !",font= 50)
+         if compinput == "Rock":
+             compimg.goto(250,0)
+             compimg.shape('rock.gif')
+         else :
+             compimg.goto(250,0)
+             compimg.shape('scissors.gif')
+        #  y.write("Ooooppps! You Lose !",font= 50)
 
 def Scissors():
      compinput= Glist[random.randint(0,2)]
+     myimg.goto(-250,0)
+     myimg.shape('scissors.gif')
      global abc
      if compinput == "Scissors":
          abc = 30 
@@ -84,7 +120,9 @@ def Scissors():
          y.goto(-490,260)
          y.write("For Rock,Paper & Scissors Press R , P & S ",font=20)
          y.goto(100,0)
-         y.write("You Won !!! ",font= 50)
+         compimg.goto(250,0)
+         compimg.shape('scissors.gif')
+        #  y.write("You Won !!! ",font= 50)
      if compinput != "Scissors":
          abc = -30
          y.clear()
@@ -93,8 +131,13 @@ def Scissors():
          y.goto(-490,260)
          y.write("For Rock,Paper & Scissors Press R , P & S ",font=20)
          y.goto(100,0)
-         y.write("Ooooppps! You Lose !",font= 50)
-
+        #  y.write("Ooooppps! You Lose !",font= 50)
+         if compinput == "Rock":
+             compimg.goto(250,0)
+             compimg.shape('rock.gif')
+         else :
+             compimg.goto(250,0)
+             compimg.shape('paper.gif')
 
 x.listen()
 x.onkey(Rock, "r")
